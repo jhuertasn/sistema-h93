c36171bde7220bea6e3713935e6348ff77243285
jhuertasn <81602342+jhuertasn@users.noreply.github.com>
2022-12-16 19:30:52 -0500

    coneccion a azure back end

--- src/app/service/helper.ts
+++ src/app/service/helper.ts
@@ -1,3 +1,3 @@
-let urlbase = 'http://localhost:8080/apirest'
-//let urlbase = 'https://proyectoh93.azurewebsites.net/apirest'
+//let urlbase = 'http://localhost:8080/apirest'
+let urlbase = 'https://proyectoh93.azurewebsites.net/apirest'
 export default urlbase;
